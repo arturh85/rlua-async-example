e084c04c02fae55b4412930a97023c6d6a2075c1
Artur Hallmann <a.hallmann@neusta.de>
2020-09-22 10:49:16 +0200

    block_on

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,26 @@
-use rlua::{Lua};
+use rlua::Lua;
 use rlua_async::{ChunkExt, ContextExt};
-
+const LUA_CODE: &str = "my.asyncfunc(42)";
 #[actix_rt::main]
-async fn main() {
-    let lua_code = "my.asyncfunc(42)";
+async fn main() -> rlua::Result<()> {
     let lua = Lua::new();
-
-    lua.context(|lua_ctx| {
+    lua.context::<_, rlua::Result<()>>(|lua_ctx| {
         let globals = lua_ctx.globals();
-        let map_table = lua_ctx.create_table().unwrap();
-        map_table
-            .set(
-                "asyncfunc",
-                lua_ctx
-                    .create_async_function(
-                        |_ctx,
-                         param:
-                            u32
-                        | async move {
-                            println!("async function called {}", param);
-                            Ok(())
-                        }).unwrap()).unwrap();
-
-        globals.set("my", map_table).unwrap();
+        let map_table = lua_ctx.create_table()?;
+        map_table.set(
+            "asyncfunc",
+            lua_ctx.create_async_function(|_ctx, param: u32| async move {
+                println!("async function called {}", param);
+                Ok(())
+            })?,
+        )?;
+        globals.set("my", map_table)?;
+        Ok(())
     });
-
-    lua.context(|lua_context| async move {
-        let chunk = lua_context
-            .load(&lua_code);
-        chunk.exec_async(lua_context).await.unwrap();
-    })
-        .await;
-    println!("finished");
+    lua.context(|lua_context| {
+        actix::System::new("Default")
+            .block_on(lua_context.load(LUA_CODE).exec_async(lua_context))
+            .unwrap();
+    });
+    Ok(())
 }
